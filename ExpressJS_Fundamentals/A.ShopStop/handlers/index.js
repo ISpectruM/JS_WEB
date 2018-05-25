9a5cbe596562d484b2a84caffb45f1cf2dcaa42d
ISpectruM <ispectrumcode@gmail.com>
2018-05-25 15:41:29 +0300

    ShopStop app

---
+++ ExpressJS_Fundamentals/A.ShopStop/handlers/index.js
@@ -0,0 +1,5 @@
+const homeHandler = require('./home');
+const filesHandler = require('./static-files.js');
+const productHandler = require('./product');
+
+module.exports = [ homeHandler,filesHandler,productHandler];
